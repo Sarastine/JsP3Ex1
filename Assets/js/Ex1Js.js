de5f1ccc57de54ec2fe53163f03d64f0da182025
Caillou <Boucher.Pierre@hotmail.fr>
2018-10-09 08:52:38 +0200

    Exercice fait

---
+++ Assets/js/Ex1Js.js
@@ -0,0 +1,7 @@
+function borderImg() {
+	/*document.getElementById('image1').border = '3px';*/
+	document.getElementById('image1').className = 'RedBorder' 
+}
+function resetBorderImg() {
+	document.getElementById('image1').className = '';
+}
